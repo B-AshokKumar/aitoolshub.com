dcf052584fccffb5325ae675f935ab69cce99e57
B-AshokKumar <ashokashkashas@gmail.com>
2026-05-01 11:11:14 +0530

    sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "ai-tools-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
+  );
+});
