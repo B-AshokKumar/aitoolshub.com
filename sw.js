8d0d4c0fa51a1573e65f7dd2f23376aded6c437c
B-AshokKumar <ashokashkashas@gmail.com>
2026-06-11 17:48:12 +0530

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ai-tools-cache-v3";
+const CACHE_NAME = "ai-tools-cache-v4";
 
 const urlsToCache = [
   "/",
@@ -7,31 +7,36 @@ const urlsToCache = [
 
 // Install
 self.addEventListener("install", event => {
-
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 
+  self.skipWaiting();
+});
+
+// Activate
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
 });
 
 // Fetch
 self.addEventListener("fetch", event => {
-
   event.respondWith(
-
     caches.match(event.request)
-      .then(response => {
-
-        return response || fetch(event.request);
-
-      })
-      .catch(() => {
-
-        return caches.match("/index.html");
-
-      })
-
+      .then(response => response || fetch(event.request))
+      .catch(() => caches.match("/index.html"))
   );
-
 });
