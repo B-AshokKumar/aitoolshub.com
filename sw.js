509fae878a8659ab46018333595ec55ca78bfddb
B-AshokKumar <ashokashkashas@gmail.com>
2026-07-20 13:00:20 +0530

    sw.js

--- sw.js
+++ sw.js
@@ -1,42 +1,84 @@
-const CACHE_NAME = "ai-tools-cache-v1.15";
+const CACHE_NAME = "ai-tools-cache-v1.16";
 
 const urlsToCache = [
   "/",
-  "/index.html"
+  "/index.html",
+  "/offline.html",
+  "/manifest.json",
+
+  "/icon-192.png",
+  "/icon-512.png",
+
+  "/about.html",
+  "/privacy-policy.html",
+  "/terms.html",
+  "/disclaimer.html"
 ];
 
 // Install
 self.addEventListener("install", event => {
+
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 
   self.skipWaiting();
+
 });
 
 // Activate
 self.addEventListener("activate", event => {
+
   event.waitUntil(
+
     caches.keys().then(cacheNames => {
+
       return Promise.all(
+
         cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
+
+          if(cache !== CACHE_NAME){
+
             return caches.delete(cache);
+
           }
+
         })
+
       );
+
     })
+
   );
 
   self.clients.claim();
+
 });
 
 // Fetch
 self.addEventListener("fetch", event => {
+
+  if (event.request.method !== "GET") return;
+  
   event.respondWith(
+
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
-      .catch(() => caches.match("/index.html"))
+      .then(response => {
+
+        return response || fetch(event.request)
+          .catch(() => {
+
+            if (event.request.mode === "navigate") {
+              return caches.match("/offline.html");
+            }
+
+            return Response.error();
+
+          });
+
+      })
+
   );
+
 });
