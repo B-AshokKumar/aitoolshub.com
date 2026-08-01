01d2df6b3fea98a5a06da01ea2868b778022f625
B-AshokKumar <ashokashkashas@gmail.com>
2026-08-01 23:41:43 +0530

    quiz.js

--- learning/js/quiz.js
+++ learning/js/quiz.js
@@ -10,7 +10,14 @@ if (quiz) {
 
     shuffledQuestions = [...quiz.questions];
 
-    shuffledQuestions.sort(() => Math.random() - 0.5);
+    for (let i = shuffledQuestions.length - 1; i > 0; i--) {
+
+    const j = Math.floor(Math.random() * (i + 1));
+
+    [shuffledQuestions[i], shuffledQuestions[j]] =
+    [shuffledQuestions[j], shuffledQuestions[i]];
+
+}
 
 }
 
