9c245e54aa1bee1b9eae679abd333372fe9f7ea3
B-AshokKumar <ashokashkashas@gmail.com>
2026-08-01 21:26:44 +0530

    quiz.js

--- learning/js/quiz.js
+++ learning/js/quiz.js
@@ -38,7 +38,14 @@ function showQuestion() {
     correct: index === q.answer
 }));
 
-options.sort(() => Math.random() - 0.5);
+// Fisher-Yates Shuffle
+for (let i = options.length - 1; i > 0; i--) {
+
+    const j = Math.floor(Math.random() * (i + 1));
+
+    [options[i], options[j]] = [options[j], options[i]];
+
+}
 
     quizProgress.textContent =
         "Question " + (currentQuestion + 1) +
