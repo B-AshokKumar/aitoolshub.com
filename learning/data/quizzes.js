1d92685f7939ec4949955462386a27027b7246cc
B-AshokKumar <ashokashkashas@gmail.com>
2026-08-01 16:49:46 +0530

    quizzes.js

--- learning/data/quizzes.js
+++ learning/data/quizzes.js
@@ -1,1 +1,74 @@
 
+const quizzes = [
+
+{
+    lessonId: 1,
+
+    questions: [
+
+        {
+            question: "What does AI stand for?",
+
+            options: [
+
+                "Artificial Intelligence",
+
+                "Automatic Internet",
+
+                "Advanced Information",
+
+                "Artificial Internet"
+
+            ],
+
+            answer: 0
+        },
+
+        {
+            question: "Which of these is an AI tool?",
+
+            options: [
+
+                "ChatGPT",
+
+                "MS Paint",
+
+                "Calculator",
+
+                "Notepad"
+
+            ],
+
+            answer: 0
+        }
+
+    ]
+},
+
+{
+    lessonId: 2,
+
+    questions: [
+
+        {
+            question: "In which year did AI become an academic field?",
+
+            options: [
+
+                "1956",
+
+                "1985",
+
+                "2000",
+
+                "1945"
+
+            ],
+
+            answer: 0
+        }
+
+    ]
+}
+
+];
