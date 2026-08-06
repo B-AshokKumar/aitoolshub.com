b315a89bc463739f708e78fd78c72e69d2bd5d09
B-AshokKumar <ashokashkashas@gmail.com>
2026-08-06 19:44:24 +0530

    study.js

--- learning/js/study.js
+++ learning/js/study.js
@@ -207,15 +207,10 @@ Complete all <strong>20 lessons</strong> and pass all
 
 function showToast(message){
 
-    alert("showToast() is working");
-
     const toast =
     document.getElementById("toast");
 
-    if(!toast){
-        alert("Toast element not found!");
-        return;
-    }
+    if(!toast) return;
 
     toast.innerHTML = message;
 
